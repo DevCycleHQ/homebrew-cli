97e3a8631f2540c8294e6049bacd9898fbc44df2
Elliot Campbell <elliotgithub@gmail.com>
2023-07-07 14:24:50 -0400

    Add formula

---
+++ Formula/Devcycle.rb
@@ -0,0 +1,16 @@
+require "language/node"
+class Devcycle < Formula
+  desc "Your package description"
+  homepage "https://github.com/DevCycleHQ/cli"
+  url "https://registry.npmjs.org/@devcycle/cli/-/cli-4.6.0.tgz"
+  sha256 "8fd5e66c8e057a71e93ec696df65a37d8c0a47b53762b7a092a4ff38c5c12133"
+
+  depends_on "node"
+
+  def install
+    cd buildpath do
+      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+      bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+  end
+end
