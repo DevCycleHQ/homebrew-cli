9dfe5ec2c308689f49109baf9addd38a9d4c891c
GitHub Action <action@github.com>
2023-10-02 15:28:35 +0000

    Update to version 5.10.5

--- Formula/Devcycle.rb
+++ Formula/Devcycle.rb
@@ -2,8 +2,8 @@ require "language/node"
 class Devcycle < Formula
   desc "Your package description"
   homepage "https://github.com/DevCycleHQ/cli"
-  url "https://registry.npmjs.org/@devcycle/cli/-/cli-5.10.4.tgz"
-  sha256 "d7cd6545de52b713d3b9e983740e1754e9dce95637be03fc9b9f6221a7d639d0"
+  url "https://registry.npmjs.org/@devcycle/cli/-/cli-5.10.5.tgz"
+  sha256 "5c150e5e438e1f77896b40d4974b6e2713d5572af320aca39db0a980c3abbc3b"
 
   depends_on "node"
 
